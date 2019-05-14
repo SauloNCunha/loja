8a9b994445e01a3b67d353c1d2f1fbe03884a1d9
Saulo Nascimento Cunha <saulonacimentocunha@hotmail.com>
2019-05-14 13:40:38 -0300

    sincronizando projetos

--- cabecalho.php
+++ cabecalho.php
@@ -6,20 +6,19 @@
     <link href="css/loja.css" rel="stylesheet" />
 </head>
 <body>
-    <div class="navbar navbar-inverse navbar-fixed-top">
-        <div class="container">
-            <div class="navbar-header">
-                <a href="index.php" class="navbar-brand">Minha Loja</a>
-            </div>
-            <div>
-                <ul class="nav navbar-nav">
-                    <li><a href="produto-formulario.php">Adiciona Produto</a></li>
-                    <li><a href="sobre.php">Sobre</a></li>
-                </ul>
-            </div>
-        </div>
-    </div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Minha Loja</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
+        <div class="navbar navbar-inverse navbar-fixed-top" id="navbarSupportedContent">
+            <ul class="nav navbar-nav">
+            <li><a href="produto-formulario.php">Adiciona Produto</a></li>
+                <li><a href="sobre.php">Sobre</a></li>
+            </ul>
+        </div>
+    </nav>
     <div class="container">
 
     <div class="principal">
