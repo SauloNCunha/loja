9e28de576ec7837d381b686424731b4e5b308ff1
SauloNCunha <saulonacimentocunha@hotmnail.com>
2019-05-26 16:38:02 -0300

    terminado projeto

--- produto-lista.php
+++ produto-lista.php
@@ -1,38 +1,23 @@
-<?php
-include("cabecalho.php");
-include("conecta.php");
-include("banco-categoria.php");
-
-$categorias = listaCategorias($conexao);
-?>
+<?php include("cabecalho.php");
+      include("conecta.php");
+      include("banco-produto.php"); ?>
 
 <?php if(array_key_exists("removido", $_GET) && $_GET['removido']=='true') { ?>
     <p class="alert-success">Produto apagado com sucesso.</p>
 <?php } ?>
 
 <table class="table table-striped table-bordered">
-    <tr>
-        <th>Nome</th>
-        <th>Valor</th>
-        <th>Descrição</th>
-        <th>Categoria</th>
-        <th>Ação</th>
-        
-    </tr>
+
     <?php
         $produtos = listaProdutos($conexao);
         foreach($produtos as $produto) :
     ?>
-   
     <tr>
         <td><?= $produto['nome'] ?></td>
         <td><?= $produto['preco'] ?></td>
         <td><?= substr($produto['descricao'], 0, 40) ?></td>
-        <td>
-            <?php foreach($categorias as $categoria) : ?>
-            <input type="radio" name="categoria_id" value="<?=$categoria['id']?>"><?=$categoria['nome']?></br>
-            <?php endforeach ?>
-        </td>
+        <td><?= $produto['categoria_nome'] ?></td>
+        <td><a class="btn btn-primary" href="produto-altera-formulario.php?id=<?=$produto['id']?>">alterar</a></td>
         <td>
             <form action="remove-produto.php" method="post">
                 <input type="hidden" name="id" value="<?=$produto['id']?>" />
